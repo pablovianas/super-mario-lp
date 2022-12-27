e41bc0ae429081b132f39117f60649e0c2055707
pablovianas <pabloviana2010@gmail.com>
2022-12-27 11:26:57 -0300

    Small layout changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,25 @@
 const modal = document.querySelector(".modal");
 const btnAbrirModal = document.querySelector(".botao-trailer");
 const btnfecharModal = document.querySelector(".fechar-modal");
+const frameSource = document.getElementById("video");
+const linkDoVideo = document.querySelector("#video").src;
 const loader = document.querySelector(".carregando");
 
-function abrirModal() {
+function alternarModal() {
     modal.classList.toggle("aberto");
 }
 
-btnAbrirModal.addEventListener("click", abrirModal);
-btnfecharModal.addEventListener("click", abrirModal);
+btnAbrirModal.addEventListener("click", ()=>{
+    frameSource.setAttribute("src", linkDoVideo);
+    alternarModal();
+});
+
+btnfecharModal.addEventListener("click", ()=>{
+    frameSource.setAttribute("src", "");
+    alternarModal();
+});
 
 setTimeout(()=>{
     loader.remove()
 },5000)
+
